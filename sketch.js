f6ec932673e162a93c77b24d1d28fe3eefaba1ac
bukva-works <74981863+bukva-works@users.noreply.github.com>
2021-01-02 23:34:57 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,22 @@ function setup() {
 }
 
 function draw() {
-  
-  if (mouseIsPressed) {
-    fill(255);
-  } else {
-    noFill();
-  }
-    rect(mouseX, mouseY, 15, 30);
-    rect(mouseX+40, mouseY, 15, 30);
+	background (255);
+	
+	fill(255)
+	ellipse (1000,540,50,200);
+	
+	fill (200,100,100);
+	stroke (0);
+	strokeWeight (2);
+	rect (mouseX,mouseY,30,160, 20);
+
+	
+	fill (255)
+	ellipse (1025,540,50,200);
+	line (1015,600,1040,570);
+	line (1015,570,1040,540);
+	line (1015,540,1040,510);
+	line (1015,510,1040,480);
+	
 }
