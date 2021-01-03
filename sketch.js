2ff017e0ad087f01dcfe9ea88b24d285e83f18df
bukva-works <74981863+bukva-works@users.noreply.github.com>
2021-01-03 13:49:13 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,40 @@
 function setup() {
-  createCanvas(1920, 1080);
+  createCanvas(windowWidth, windowHeight);
 }
 
+let width = 20;
+let height = 20;
+let color = 0;
+let stroke = 0;
+let back = 0;
 function draw() {
-	
-	background(255);
-	fill(255);
-	ellipse (1000,540,50,200);
-	
-	fill (200,100,100);
-	stroke (0);
-	strokeWeight (2);
-	rect (mouseX,mouseY,30,160, 20);
+  if (mouseIsPressed) {
+    fill(color,100);
+    strokeWeight(stroke)
+  } else {
+    fill(color,0);
+    strokeWeight(0)
+  }
 
-	
-	fill (255)
-	ellipse (1025,540,50,200);
-	line (1015,600,1040,570);
-	line (1015,570,1040,540);
-	line (1015,540,1040,510);
-	line (1015,510,1040,480);
-	
+  ellipse(mouseX, mouseY, width, height);
+
+  background(255,back);
+}
+function keyTyped() {
+  if (key === 'w') {
+    color = 255;
+    stroke = 1;
+  } else if (key === 'b') {
+    color = 0;
+    stroke = 0;
+  }
+  
+  if (key === 'e') {
+    width = width+10;
+    height = height+10;
+  } else if (key === 'q') {
+    width = width-10;
+    height = height-10;
+  }
+  
 }
